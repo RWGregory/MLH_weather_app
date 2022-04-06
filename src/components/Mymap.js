a3f1c3ffa3f6d15d91027efe9bc2241fd624e465
RWGregory <91198091+RWGregory@users.noreply.github.com>
2022-04-06 12:55:31 -0400

    corrected casing for MyMap component

--- src/components/Mymap.js
+++ src/components/Mymap.js
@@ -1,40 +1,41 @@
 import React from 'react'
-import mapboxgl from "mapbox-gl";
+import mapboxgl from 'mapbox-gl'
 
+function MyMap({
+  latLng,
+  setLatLng,
+  setCity,
+  results,
+  setResults,
+  setClickedLast,
+}) {
+  const mapContainer = React.useRef(null)
+  const map = React.useRef(null)
+  const marker = React.useRef(null)
 
-
-function MyMap({latLng, setLatLng, setCity, results, setResults, setClickedLast}) {
-  const mapContainer = React.useRef(null);
-  const map = React.useRef(null);
-  const marker=React.useRef(null);
-  
   mapboxgl.accessToken = `${process.env.REACT_APP_MAPBOXGL}`
-    
-    React.useEffect(() => {
-       
-      map.current = new mapboxgl.Map({
-          container: mapContainer.current, // container ID
-          style: 'mapbox://styles/mapbox/streets-v11', // style URL
-          center: [latLng[1], latLng[0]], // starting position [lng, lat] <--
-          zoom: 9 // starting zoom
-          })
-          .on('click', (e) => {
-            setLatLng([{...e.lngLat}.lat, {...e.lngLat}.lng])
-            setClickedLast(true)
-          })
-        
-          marker.current = new mapboxgl.Marker()
-            .setLngLat([latLng[1], latLng[0]])//this is a mapbox method- it doesn't change a declared state variable
-            .addTo(map.current)
 
-        }, [latLng]);
+  React.useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [latLng[1], latLng[0]], // starting position [lng, lat] <--
+      zoom: 9, // starting zoom
+    }).on('click', (e) => {
+      setLatLng([{ ...e.lngLat }.lat, { ...e.lngLat }.lng])
+      setClickedLast(true)
+    })
 
-    return (
-        <div>
-          <div className='map-container' ref={mapContainer} />
-        </div>  
-      )
+    marker.current = new mapboxgl.Marker()
+      .setLngLat([latLng[1], latLng[0]]) //this is a mapbox method- it doesn't change a declared state variable
+      .addTo(map.current)
+  }, [latLng])
+
+  return (
+    <div>
+      <div className="map-container" ref={mapContainer} />
+    </div>
+  )
 }
 
-
-export default MyMap 
+export default MyMap
